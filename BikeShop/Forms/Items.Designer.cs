07a86cd85085f0854af10125587d58844dc05e65
Kevin <johnk@KEVIN>
2025-11-16 15:54:37 +0800

    Added new buttons, Product and Accessories

--- BikeShop/Forms/Items.Designer.cs
+++ BikeShop/Forms/Items.Designer.cs
@@ -28,8 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Items));
             DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Items));
             panel1 = new Panel();
             btlogout = new Button();
             button1 = new Button();
@@ -39,17 +39,15 @@
             label2 = new Label();
             textBox1 = new TextBox();
             label3 = new Label();
-            label4 = new Label();
-            textBox2 = new TextBox();
             button4 = new Button();
             dataGridView1 = new DataGridView();
             label5 = new Label();
             textBox3 = new TextBox();
             checkBox1 = new CheckBox();
             checkBox2 = new CheckBox();
-            button5 = new Button();
             label6 = new Label();
             numericUpDown1 = new NumericUpDown();
+            button2 = new Button();
             button3 = new Button();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
@@ -61,6 +59,8 @@
             // panel1
             // 
             panel1.BackColor = Color.Silver;
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
             panel1.Controls.Add(btlogout);
             panel1.Controls.Add(button1);
             panel1.Controls.Add(pictureBox1);
@@ -93,7 +93,7 @@
             button1.ForeColor = Color.Black;
             button1.Image = (Image)resources.GetObject("button1.Image");
             button1.ImageAlign = ContentAlignment.MiddleLeft;
-            button1.Location = new Point(0, 98);
+            button1.Location = new Point(0, 107);
             button1.Name = "button1";
             button1.Size = new Size(169, 49);
             button1.TabIndex = 3;
@@ -133,17 +133,17 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label2.Location = new Point(181, 72);
             label2.Name = "label2";
-            label2.Size = new Size(109, 21);
+            label2.Size = new Size(132, 25);
             label2.TabIndex = 2;
             label2.Text = "Bike Category:";
             // 
             // textBox1
             // 
             textBox1.BorderStyle = BorderStyle.FixedSingle;
-            textBox1.Location = new Point(397, 98);
+            textBox1.Location = new Point(355, 100);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(188, 23);
             textBox1.TabIndex = 3;
@@ -151,37 +151,19 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label3.Location = new Point(397, 72);
+            label3.Font = new Font("Segoe UI", 14.25F);
+            label3.Location = new Point(355, 74);
             label3.Name = "label3";
-            label3.Size = new Size(80, 21);
+            label3.Size = new Size(97, 25);
             label3.TabIndex = 5;
             label3.Text = "Item Type:";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label4.Location = new Point(828, 72);
-            label4.Name = "label4";
-            label4.Size = new Size(47, 21);
-            label4.TabIndex = 7;
-            label4.Text = "Price:";
-            // 
-            // textBox2
-            // 
-            textBox2.BorderStyle = BorderStyle.FixedSingle;
-            textBox2.Location = new Point(828, 98);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(188, 23);
-            textBox2.TabIndex = 6;
             // 
             // button4
             // 
             button4.FlatAppearance.BorderColor = Color.Black;
-            button4.Location = new Point(181, 139);
+            button4.Location = new Point(355, 143);
             button4.Name = "button4";
-            button4.Size = new Size(156, 26);
+            button4.Size = new Size(188, 26);
             button4.TabIndex = 8;
             button4.Text = "Add Item";
             button4.UseVisualStyleBackColor = true;
@@ -208,17 +190,17 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label5.Location = new Point(612, 71);
+            label5.Font = new Font("Segoe UI", 14.25F);
+            label5.Location = new Point(570, 73);
             label5.Name = "label5";
-            label5.Size = new Size(90, 21);
+            label5.Size = new Size(108, 25);
             label5.TabIndex = 11;
             label5.Text = "Item Name:";
             // 
             // textBox3
             // 
             textBox3.BorderStyle = BorderStyle.FixedSingle;
-            textBox3.Location = new Point(612, 98);
+            textBox3.Location = new Point(570, 100);
             textBox3.Name = "textBox3";
             textBox3.Size = new Size(188, 23);
             textBox3.TabIndex = 10;
@@ -226,10 +208,11 @@
             // checkBox1
             // 
             checkBox1.AutoSize = true;
+            checkBox1.Font = new Font("Segoe UI", 14.25F);
             checkBox1.Location = new Point(188, 100);
-            checkBox1.Margin = new Padding(2, 2, 2, 2);
+            checkBox1.Margin = new Padding(2);
             checkBox1.Name = "checkBox1";
-            checkBox1.Size = new Size(78, 19);
+            checkBox1.Size = new Size(113, 29);
             checkBox1.TabIndex = 12;
             checkBox1.Text = "Mountain";
             checkBox1.UseVisualStyleBackColor = true;
@@ -237,50 +220,61 @@
             // checkBox2
             // 
             checkBox2.AutoSize = true;
-            checkBox2.Location = new Point(286, 100);
-            checkBox2.Margin = new Padding(2, 2, 2, 2);
+            checkBox2.Font = new Font("Segoe UI", 14.25F);
+            checkBox2.Location = new Point(188, 127);
+            checkBox2.Margin = new Padding(2);
             checkBox2.Name = "checkBox2";
-            checkBox2.Size = new Size(53, 19);
+            checkBox2.Size = new Size(73, 29);
             checkBox2.TabIndex = 13;
             checkBox2.Text = "Road";
             checkBox2.UseVisualStyleBackColor = true;
             // 
-            // button5
-            // 
-            button5.Location = new Point(764, 139);
-            button5.Margin = new Padding(2, 2, 2, 2);
-            button5.Name = "button5";
-            button5.Size = new Size(113, 26);
-            button5.TabIndex = 14;
-            button5.Text = "Add Qty";
-            button5.UseVisualStyleBackColor = true;
-            // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label6.Location = new Point(397, 121);
+            label6.Font = new Font("Segoe UI", 14.25F);
+            label6.Location = new Point(779, 73);
             label6.Name = "label6";
-            label6.Size = new Size(108, 21);
+            label6.Size = new Size(130, 25);
             label6.TabIndex = 16;
             label6.Text = "Item Quantity:";
             // 
             // numericUpDown1
             // 
-            numericUpDown1.Location = new Point(397, 143);
-            numericUpDown1.Margin = new Padding(2, 2, 2, 2);
+            numericUpDown1.Location = new Point(779, 100);
+            numericUpDown1.Margin = new Padding(2);
             numericUpDown1.Name = "numericUpDown1";
             numericUpDown1.Size = new Size(188, 23);
             numericUpDown1.TabIndex = 17;
             // 
+            // button2
+            // 
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.ForeColor = Color.Black;
+            button2.Image = (Image)resources.GetObject("button2.Image");
+            button2.ImageAlign = ContentAlignment.MiddleLeft;
+            button2.Location = new Point(0, 162);
+            button2.Name = "button2";
+            button2.Size = new Size(169, 49);
+            button2.TabIndex = 5;
+            button2.Text = "Products";
+            button2.UseVisualStyleBackColor = true;
+            // 
             // button3
             // 
-            button3.Location = new Point(903, 139);
-            button3.Margin = new Padding(2, 2, 2, 2);
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.ForeColor = Color.Black;
+            button3.Image = (Image)resources.GetObject("button3.Image");
+            button3.ImageAlign = ContentAlignment.MiddleLeft;
+            button3.Location = new Point(0, 217);
             button3.Name = "button3";
-            button3.Size = new Size(113, 26);
-            button3.TabIndex = 18;
-            button3.Text = "Deduct Qty";
+            button3.Size = new Size(169, 49);
+            button3.TabIndex = 6;
+            button3.Text = "Accessories";
             button3.UseVisualStyleBackColor = true;
             // 
             // Items
@@ -288,18 +282,14 @@
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1082, 599);
-            Controls.Add(button3);
             Controls.Add(numericUpDown1);
             Controls.Add(label6);
-            Controls.Add(button5);
             Controls.Add(checkBox2);
             Controls.Add(checkBox1);
             Controls.Add(label5);
             Controls.Add(textBox3);
             Controls.Add(dataGridView1);
             Controls.Add(button4);
-            Controls.Add(label4);
-            Controls.Add(textBox2);
             Controls.Add(label3);
             Controls.Add(textBox1);
             Controls.Add(label2);
@@ -329,17 +319,15 @@
         private Label label2;
         private TextBox textBox1;
         private Label label3;
-        private Label label4;
-        private TextBox textBox2;
         private Button button4;
         private DataGridView dataGridView1;
         private Label label5;
         private TextBox textBox3;
         private CheckBox checkBox1;
         private CheckBox checkBox2;
-        private Button button5;
         private Label label6;
         private NumericUpDown numericUpDown1;
         private Button button3;
+        private Button button2;
     }
 }
